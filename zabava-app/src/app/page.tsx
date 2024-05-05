dc79df4b4ab140fc88586e072402a79b42a27339
Clodoaldo Sodré Fh <115301755+Closo99@users.noreply.github.com>
2024-05-05 04:15:28 -0300

    Adding AppBar component and updating favicon

--- zabava-app/src/app/page.tsx
+++ zabava-app/src/app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import PrimaryAppBar from "./components/AppBar";
 
 export default function Home() {
   return (
     <main className='home-main'>
-      
+      <div><PrimaryAppBar></PrimaryAppBar></div>
     </main>
   );
 }
