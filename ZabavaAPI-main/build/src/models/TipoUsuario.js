bac5d7177495c35281b347b674b8f0e7d1648c5f
Clodoaldo Sodré Fh <115301755+Closo99@users.noreply.github.com>
2024-06-08 15:14:03 -0300

    Revert "Adding ZabavaAPI"
    This reverts commit 3acab7ce933d9e8a3667302f30d5ae471f96e164.

--- ZabavaAPI-main/build/src/models/TipoUsuario.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TipoUsuario = void 0;
-var TipoUsuario;
-(function (TipoUsuario) {
-    TipoUsuario["PROPRIETARIO"] = "PROPRIETARIO";
-    TipoUsuario["PRODUTOR"] = "PRODUTOR";
-    TipoUsuario["STAFF"] = "STAFF";
-})(TipoUsuario || (exports.TipoUsuario = TipoUsuario = {}));
--- ZabavaAPI-main/src/models/trabalho.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-const trabalhoSchema = new mongoose.Schema({
-  funcao: { type: String, required: true },
-  disponibilidade: { type: String, required: true },
-});
-
-export const TrabalhoModel = mongoose.model('Trabalho', trabalhoSchema);
